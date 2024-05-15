5074f7647029166d1d64c43b02a09e0855b80563
DavidValente05 <122547318+DavidValente05@users.noreply.github.com>
2024-05-15 14:40:10 +0100

    Updated microsimulation files

--- Microsimulator/ROBOT.py
+++ Microsimulator/ROBOT.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import numpy as np
+import time
 
 
 # calculates distance between 2 points
@@ -28,8 +29,9 @@ class Robot:
 
         self.min_obs_dist = 100 # pixels
         self.countdown = 5 # seconds
-
+    
     def avoid_obstacles(self, point_cloud, dt):
+
         closest_obs = None # initial closest obstacle to the robot 
         dist = np.inf # initial distance to the closest obstacle (infinite)
 
@@ -47,24 +49,32 @@ class Robot:
                     self.move_forward()
 
     def move_backward(self):
-        self.vr = - self.minspeed
-        self.vl = - self.minspeed/2
+        self.vr = - 1.5*self.minspeed 
+        self.vl = - 1.5*self.minspeed #/2
 
     def move_forward(self):
-        self.vr = self.minspeed
-        self.vl = self.minspeed
+        self.vr = 1.5*self.minspeed
+        self.vl = 1.5*self.minspeed
+
+    def move_left(self):
+        self.vr =  self.minspeed 
+        self.vl = - self.minspeed 
+
+    def move_right(self):
+        self.vr = - self.minspeed 
+        self.vl =  self.minspeed 
 
     def kinematics(self, dt):
         self.x += ((self.vl+self.vr)/2) * math.cos(self.heading) * dt
         self.y -= ((self.vl+self.vr)/2) * math.sin(self.heading) * dt # the computer frame is inverted
+        
         self.heading += (self.vr - self.vl) / self.w * dt
 
-        if self.heading > 2*math.pi or self.heading < -2*math.pi:
+        if self.heading > 2*math.pi or self.heading < -2*math.pi: 
             self.heading = 0
 
-        self.vr = max(min(self.maxspeed, self.vr), self.minspeed)
-        self.vl = max(min(self.maxspeed, self.vl), self.minspeed)
-
+        self.vr = max(min(self.maxspeed, self.vr), self.minspeed) #limit velocity
+        self.vl = max(min(self.maxspeed, self.vl), self.minspeed) #limit velocity
 
 class Graphics:
     def __init__(self, dimensions, robot_img_path, map_img_path):
@@ -88,7 +98,7 @@ class Graphics:
         self.height, self.width = dimensions
 
         # window settings
-        pygame.display.set_caption("Obstacle Avoidance")
+        pygame.display.set_caption("Turtlebot Mapping - Group 13") #it used to be "Obstacle Avoidance"
         self.map = pygame.display.set_mode((self.width, self.height))
         self.map.blit(self.map_img, (0, 0))
 
@@ -99,9 +109,37 @@ class Graphics:
 
     def draw_sensor_data(self, point_cloud):
         for point in point_cloud:
-            pygame.draw.circle(self.map, self.red, point, 3, 0)
+            pygame.draw.circle(self.map, self.red, point, 3, 0) #color of obstacle detected
 
+class Lidar:
+    def __init__(self, sensor_range, map):
+        self.sensor_range = sensor_range
+        self.map_width, self.map_height = pygame.display.get_surface().get_size()
+        self.map = map
 
+    def sense_obstacles(self, x, y, heading):
+        obstacles = []
+        x1, y1 = x, y
+        start_angle = heading - self.sensor_range[1]
+        finish_angle = heading + self.sensor_range[1]
+        n_beams = 360
+        for angle in np.linspace(start_angle, finish_angle, n_beams, False):
+            x2 = x1 + self.sensor_range[0] * math.cos(angle)
+            y2 = y1 - self.sensor_range[0] * math.sin(angle)
+            n_beam_ponits = 100
+            for i in range (0, n_beam_ponits):
+                u = i / n_beam_ponits
+                x = int(x2*u + x1*(1 - u))
+                y = int(y2*u + y1*(1 - u))
+                if 0 < x < self.map_width and 0 < y < self.map_height: # sample is inside the map
+                    color = self.map.get_at((x, y))
+                    self.map.set_at((x, y), (0, 208, 255)) # color of beam 
+                    if (color[0], color[1], color[2]) == (0, 0, 0): # there is an obstacle
+                        obstacles.append([x, y])
+                        break
+        return obstacles
+
+"""
 class Ultrasonic:
     def __init__(self, sensor_range, map):
         self.sensor_range = sensor_range
@@ -113,7 +151,7 @@ class Ultrasonic:
         x1, y1 = x, y
         start_angle = heading - self.sensor_range[1]
         finish_angle = heading + self.sensor_range[1]
-        for angle in np.linspace(start_angle, finish_angle, 10, False):
+        for angle in np.linspace(start_angle, finish_angle, 40, False):
             x2 = x1 + self.sensor_range[0] * math.cos(angle)
             y2 = y1 - self.sensor_range[0] * math.sin(angle)
             for i in range (0, 100):
@@ -127,3 +165,4 @@ class Ultrasonic:
                         obstacles.append([x, y])
                         break
         return obstacles
+"""
--- Microsimulator/main.py
+++ Microsimulator/main.py
@@ -1,7 +1,7 @@
 import math
 import pygame
-
-from ROBOT import Graphics, Robot, Ultrasonic
+import time
+from ROBOT import Graphics, Robot, Lidar#, Ultrasonic
 
 MAP_DIMENSIONS = (600, 1200)
 
@@ -12,9 +12,11 @@ gfx = Graphics(MAP_DIMENSIONS, 'DDR.png', 'ObstacleMap.png')
 start = (200, 200)
 robot = Robot(start, 0.01*3779.52)
 
+bearing=360
 # sensor
-sensor_range = 250, math.radians(40)
-ultra_sonic = Ultrasonic(sensor_range, gfx.map)
+sensor_range = 250, math.radians(bearing/2) #250,rad(40)
+#ultra_sonic = Ultrasonic(sensor_range, gfx.map)
+lidar = Lidar(sensor_range, gfx.map)
 
 dt = 0
 last_time = pygame.time.get_ticks()
@@ -22,20 +24,49 @@ last_time = pygame.time.get_ticks()
 running = True
 
 # simulation loop
+
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_w]:
+            robot.move_forward()
+            robot.kinematics(dt)
+            print(robot.x)
+            print(robot.y)
+            print(robot.heading)
+        if keys[pygame.K_s]:
+            robot.move_backward()
+            robot.kinematics(dt)
+            print(robot.x)
+            print(robot.y)
+            print(robot.heading)
+        if keys[pygame.K_a]:
+            robot.move_left()
+            robot.kinematics(dt)
+            print(robot.x)
+            print(robot.y)
+            print(robot.heading)
+        if keys[pygame.K_d]:
+            robot.move_right()
+            robot.kinematics(dt)
+            print(robot.x)
+            print(robot.y)
+            print(robot.heading)
+    
     dt = (pygame.time.get_ticks() - last_time)/1000
     last_time = pygame.time.get_ticks()
-
+    
     gfx.map.blit(gfx.map_img, (0, 0))
 
-    robot.kinematics(dt)
+    #robot.kinematics(dt)
     gfx.draw_robot(robot.x, robot.y, robot.heading)
-    point_cloud = ultra_sonic.sense_obstacles(robot.x, robot.y, robot.heading)
-    robot.avoid_obstacles(point_cloud, dt)
+    #point_cloud = ultra_sonic.sense_obstacles(robot.x, robot.y, robot.heading)
+    point_cloud = lidar.sense_obstacles(robot.x, robot.y, robot.heading)
+    #robot.avoid_obstacles(point_cloud, dt)
     gfx.draw_sensor_data(point_cloud)
 
     pygame.display.update()
